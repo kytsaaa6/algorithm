568e625a402e5bca4818da0136d55e350d3b7a4d
kytsaaa6 <kytsaaa6@naver.com>
2021-11-22 02:27:49 +0900

    기타

--- src/main/java/kym/study/algorithm/doit/chap03/GenericClassTester.java
+++ src/main/java/kym/study/algorithm/doit/chap03/GenericClassTester.java
@@ -1,2 +1,24 @@
-package kym.study.algorithm.doit.chap03;public class GenericClassTester {
+package kym.study.algorithm.doit.chap03;
+// 제네릭 클래스의 예
+public class GenericClassTester {
+    // 제네릭 클래스의 파리미터를 T라고 작성합니다.
+    static class GenericClass<T> {
+        private T xyz;
+        GenericClass(T t) {     // 생성자
+            this.xyz = t;
+        }
+
+        T getXyz() {            // xyz 반환
+            return xyz;
+        }
+    }
+
+    public static void main(String[] args) {
+        GenericClass<String> s = new GenericClass<String>("ABC");
+        GenericClass<Integer> n = new GenericClass<Integer>(15);
+
+        System.out.println(s.getXyz());
+        System.out.println(n.getXyz());
+
+    }
 }
